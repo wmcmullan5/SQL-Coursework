70f40c47dc5b6e2f33d1cb8a9e9db68a2668d7b9
wmcmullan5 <williammcmullan374@gmail.com>
2025-02-17 14:53:29 +0000

    login screen

--- SQL Coursework/Login.Designer.cs
+++ SQL Coursework/Login.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Login
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,178 +13,110 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.registerbtn = new System.Windows.Forms.PictureBox();
-            this.loginbtn = new System.Windows.Forms.PictureBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.usernamelogintxtbox = new System.Windows.Forms.TextBox();
-            this.passlogintxtbox = new System.Windows.Forms.TextBox();
-            this.usernameregtxtbox = new System.Windows.Forms.TextBox();
-            this.emailtxtbox = new System.Windows.Forms.TextBox();
-            this.jobtxtbox = new System.Windows.Forms.TextBox();
-            this.PassRegtxtbox = new System.Windows.Forms.TextBox();
-            this.idtxtbox = new System.Windows.Forms.TextBox();
-            this.cpasstxtbox = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.registerbtn)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.loginbtn)).BeginInit();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabLogin = new System.Windows.Forms.TabPage();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtLoginPass = new System.Windows.Forms.TextBox();
+            this.txtLoginUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabRegister = new System.Windows.Forms.TabPage();
+            this.txtRegAdmin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtRegJob = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtRegConfirm = new System.Windows.Forms.TextBox();
+            this.txtRegPass = new System.Windows.Forms.TextBox();
+            this.txtRegUser = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabLogin.SuspendLayout();
+            this.tabRegister.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // registerbtn
-            // 
-            this.registerbtn.BackgroundImage = global::SQL_Coursework.Properties.Resources.Heading;
-            this.registerbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.registerbtn.Location = new System.Drawing.Point(673, 363);
-            this.registerbtn.Name = "registerbtn";
-            this.registerbtn.Size = new System.Drawing.Size(93, 45);
-            this.registerbtn.TabIndex = 3;
-            this.registerbtn.TabStop = false;
-            this.registerbtn.Click += new System.EventHandler(this.registerbtn_Click);
-            this.registerbtn.MouseLeave += new System.EventHandler(this.SwitchBtnBack);
-            this.registerbtn.MouseHover += new System.EventHandler(this.SwitchBtn);
-            // 
-            // loginbtn
-            // 
-            this.loginbtn.BackgroundImage = global::SQL_Coursework.Properties.Resources.Heading;
-            this.loginbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.loginbtn.Location = new System.Drawing.Point(45, 229);
-            this.loginbtn.Name = "loginbtn";
-            this.loginbtn.Size = new System.Drawing.Size(78, 43);
-            this.loginbtn.TabIndex = 4;
-            this.loginbtn.TabStop = false;
-            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
-            this.loginbtn.MouseLeave += new System.EventHandler(this.SwitchBtnBack);
-            this.loginbtn.MouseHover += new System.EventHandler(this.SwitchBtn);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
-            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.checkBox1.Location = new System.Drawing.Point(64, 288);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 5;
-            this.checkBox1.UseVisualStyleBackColor = false;
-            // 
-            // usernamelogintxtbox
-            // 
-            this.usernamelogintxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.usernamelogintxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.usernamelogintxtbox.Location = new System.Drawing.Point(54, 119);
-            this.usernamelogintxtbox.Name = "usernamelogintxtbox";
-            this.usernamelogintxtbox.Size = new System.Drawing.Size(206, 16);
-            this.usernamelogintxtbox.TabIndex = 6;
-            // 
-            // passlogintxtbox
-            // 
-            this.passlogintxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.passlogintxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.passlogintxtbox.Location = new System.Drawing.Point(54, 192);
-            this.passlogintxtbox.Name = "passlogintxtbox";
-            this.passlogintxtbox.Size = new System.Drawing.Size(206, 16);
-            this.passlogintxtbox.TabIndex = 7;
-            this.passlogintxtbox.UseSystemPasswordChar = true;
-            // 
-            // usernameregtxtbox
-            // 
-            this.usernameregtxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.usernameregtxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.usernameregtxtbox.Location = new System.Drawing.Point(339, 119);
-            this.usernameregtxtbox.Name = "usernameregtxtbox";
-            this.usernameregtxtbox.Size = new System.Drawing.Size(206, 16);
-            this.usernameregtxtbox.TabIndex = 8;
-            // 
-            // emailtxtbox
-            // 
-            this.emailtxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.emailtxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.emailtxtbox.Location = new System.Drawing.Point(582, 119);
-            this.emailtxtbox.Name = "emailtxtbox";
-            this.emailtxtbox.Size = new System.Drawing.Size(206, 16);
-            this.emailtxtbox.TabIndex = 9;
-            // 
-            // jobtxtbox
-            // 
-            this.jobtxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.jobtxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.jobtxtbox.Location = new System.Drawing.Point(582, 201);
-            this.jobtxtbox.Name = "jobtxtbox";
-            this.jobtxtbox.Size = new System.Drawing.Size(206, 16);
-            this.jobtxtbox.TabIndex = 11;
-            // 
-            // PassRegtxtbox
-            // 
-            this.PassRegtxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.PassRegtxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.PassRegtxtbox.Location = new System.Drawing.Point(339, 201);
-            this.PassRegtxtbox.Name = "PassRegtxtbox";
-            this.PassRegtxtbox.Size = new System.Drawing.Size(206, 16);
-            this.PassRegtxtbox.TabIndex = 10;
-            // 
-            // idtxtbox
-            // 
-            this.idtxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.idtxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.idtxtbox.Location = new System.Drawing.Point(582, 277);
-            this.idtxtbox.Name = "idtxtbox";
-            this.idtxtbox.Size = new System.Drawing.Size(206, 16);
-            this.idtxtbox.TabIndex = 13;
-            // 
-            // cpasstxtbox
-            // 
-            this.cpasstxtbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
-            this.cpasstxtbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.cpasstxtbox.Location = new System.Drawing.Point(339, 277);
-            this.cpasstxtbox.Name = "cpasstxtbox";
-            this.cpasstxtbox.Size = new System.Drawing.Size(206, 16);
-            this.cpasstxtbox.TabIndex = 12;
-            // 
-            // Login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::SQL_Coursework.Properties.Resources.Storyboards__2_;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.idtxtbox);
-            this.Controls.Add(this.cpasstxtbox);
-            this.Controls.Add(this.jobtxtbox);
-            this.Controls.Add(this.PassRegtxtbox);
-            this.Controls.Add(this.emailtxtbox);
-            this.Controls.Add(this.usernameregtxtbox);
-            this.Controls.Add(this.passlogintxtbox);
-            this.Controls.Add(this.usernamelogintxtbox);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.loginbtn);
-            this.Controls.Add(this.registerbtn);
-            this.Name = "Login";
-            this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.registerbtn)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.loginbtn)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
 
+            // Tab Control
+            this.tabControl1.Controls.Add(this.tabLogin);
+            this.tabControl1.Controls.Add(this.tabRegister);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(400, 300);
+            this.tabControl1.TabIndex = 0;
+
+            // Login Tab
+            this.tabLogin.Controls.Add(this.btnLogin);
+            this.tabLogin.Controls.Add(this.txtLoginPass);
+            this.tabLogin.Controls.Add(this.txtLoginUser);
+            this.tabLogin.Controls.Add(this.label2);
+            this.tabLogin.Controls.Add(this.label1);
+            this.tabLogin.Location = new System.Drawing.Point(4, 22);
+            this.tabLogin.Name = "tabLogin";
+            this.tabLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLogin.Size = new System.Drawing.Size(392, 274);
+            this.tabLogin.TabIndex = 0;
+            this.tabLogin.Text = "Login";
+            this.tabLogin.UseVisualStyleBackColor = true;
+
+            // Register Tab
+            this.tabRegister.Controls.Add(this.txtRegAdmin);
+            this.tabRegister.Controls.Add(this.label7);
+            this.tabRegister.Controls.Add(this.txtRegJob);
+            this.tabRegister.Controls.Add(this.label6);
+            this.tabRegister.Controls.Add(this.btnRegister);
+            this.tabRegister.Controls.Add(this.txtRegConfirm);
+            this.tabRegister.Controls.Add(this.txtRegPass);
+            this.tabRegister.Controls.Add(this.txtRegUser);
+            this.tabRegister.Controls.Add(this.label5);
+            this.tabRegister.Controls.Add(this.label4);
+            this.tabRegister.Controls.Add(this.label3);
+            this.tabRegister.Location = new System.Drawing.Point(4, 22);
+            this.tabRegister.Name = "tabRegister";
+            this.tabRegister.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRegister.Size = new System.Drawing.Size(392, 274);
+            this.tabRegister.TabIndex = 1;
+            this.tabRegister.Text = "Register";
+            this.tabRegister.UseVisualStyleBackColor = true;
+
+            // ... (Add all controls with proper positions and sizes)
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "Authentication System";
+            this.tabControl1.ResumeLayout(false);
+            this.tabLogin.ResumeLayout(false);
+            this.tabLogin.PerformLayout();
+            this.tabRegister.ResumeLayout(false);
+            this.tabRegister.PerformLayout();
+            this.ResumeLayout(false);
         }
 
-        #endregion
-
-        private PictureBox registerbtn;
-        private PictureBox loginbtn;
-        private CheckBox checkBox1;
-        private TextBox usernamelogintxtbox;
-        private TextBox passlogintxtbox;
-        private TextBox usernameregtxtbox;
-        private TextBox emailtxtbox;
-        private TextBox jobtxtbox;
-        private TextBox PassRegtxtbox;
-        private TextBox idtxtbox;
-        private TextBox cpasstxtbox;
+        private TabControl tabControl1;
+        private TabPage tabLogin;
+        private TabPage tabRegister;
+        private Button btnLogin;
+        private TextBox txtLoginPass;
+        private TextBox txtLoginUser;
+        private Label label2;
+        private Label label1;
+        private TextBox txtRegAdmin;
+        private Label label7;
+        private TextBox txtRegJob;
+        private Label label6;
+        private Button btnRegister;
+        private TextBox txtRegConfirm;
+        private TextBox txtRegPass;
+        private TextBox txtRegUser;
+        private Label label5;
+        private Label label4;
+        private Label label3;
     }
 }
--- SQL Coursework/Login.cs
+++ SQL Coursework/Login.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,8 +9,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing;
-using System.Drawing.Drawing2D;
 
 namespace SQL_Coursework
 {
@@ -18,25 +18,128 @@ namespace SQL_Coursework
         {
             InitializeComponent();
         }
-        //switched to highlighted button
-        private void SwitchBtn(object sender, EventArgs e)
+       private void btnLogin_Click(object sender, EventArgs e)
         {
-            registerbtn.Image = Properties.Resources.Heading__1_;
+            string username = txtLoginUser.Text;
+            string password = txtLoginPass.Text;
+
+            if (ValidateLogin(username, password))
+            {
+                MessageBox.Show("Login successful!");
+                // Open next form here
+                Menu next = new Menu();
+                next.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password");
+            }
         }
-        //switches back to regular button
-        private void SwitchBtnBack(object sender, EventArgs e)
+
+        private bool ValidateLogin(string username, string password)
         {
-            registerbtn.Image = Properties.Resources.Heading;
+            using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.UserAuthConnectionString))
+            {
+                conn.Open();
+                string hashedPassword = HashPassword(password);
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @User AND Password = @Pass";
+                
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@User", username);
+                    cmd.Parameters.AddWithValue("@Pass", hashedPassword);
+                    int result = (int)cmd.ExecuteScalar();
+                    return result > 0;
+                }
+            }
         }
 
-        private void registerbtn_Click(object sender, EventArgs e)
+        private void btnRegister_Click(object sender, EventArgs e)
         {
-
+            if (ValidateRegistration())
+            {
+                if (RegisterUser())
+                {
+                    MessageBox.Show("Registration successful!");
+                    tabControl1.SelectedIndex = 0; // Switch to login tab
+                }
+            }
         }
 
-        private void loginbtn_Click(object sender, EventArgs e)
+        private bool ValidateRegistration()
         {
+            // Check all fields
+            if (string.IsNullOrWhiteSpace(txtRegUser.Text) || 
+                string.IsNullOrWhiteSpace(txtRegPass.Text) ||
+                string.IsNullOrWhiteSpace(txtRegJob.Text))
+            {
+                MessageBox.Show("Please fill all required fields");
+                return false;
+            }
 
+            // Check password match
+            if (txtRegPass.Text != txtRegConfirm.Text)
+            {
+                MessageBox.Show("Passwords do not match");
+                return false;
+            }
+
+            // Check password complexity
+            if (!Regex.IsMatch(txtRegPass.Text, @"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$"))
+            {
+                MessageBox.Show("Password must contain:\n- At least 8 characters\n- 1 uppercase letter\n- 1 number\n- 1 special character");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool RegisterUser()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.UserAuthConnectionString))
+                {
+                    conn.Open();
+                    string query = @"INSERT INTO Users (Username, Password, JobTitle, AdminID) 
+                                   VALUES (@User, @Pass, @Job, @Admin)";
+                    
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@User", txtRegUser.Text);
+                        cmd.Parameters.AddWithValue("@Pass", HashPassword(txtRegPass.Text));
+                        cmd.Parameters.AddWithValue("@Job", txtRegJob.Text);
+                        cmd.Parameters.AddWithValue("@Admin", string.IsNullOrWhiteSpace(txtRegAdmin.Text) ? 
+                            (object)DBNull.Value : txtRegAdmin.Text);
+
+                        int result = cmd.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2627) // Primary key violation
+                {
+                    MessageBox.Show("Username already exists");
+                }
+                else
+                {
+                    MessageBox.Show("Registration error: " + ex.Message);
+                }
+                return false;
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
         }
     }
 }
+
