891f4e6d89007f7adb09db87f2725cf1bce5dd0a
wmcmullan5 <williammcmullan374@gmail.com>
2025-03-06 11:00:30 +0000

    updates

--- SQL Coursework/AddEntry.Designer.cs
+++ SQL Coursework/AddEntry.Designer.cs
@@ -29,17 +29,32 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.UploadEntryBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(168)))), ((int)(((byte)(124)))));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(118, 88);
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 23);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // UploadEntryBtn
+            // 
+            this.UploadEntryBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
+            this.UploadEntryBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.UploadEntryBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UploadEntryBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UploadEntryBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UploadEntryBtn.Location = new System.Drawing.Point(312, 348);
+            this.UploadEntryBtn.Name = "UploadEntryBtn";
+            this.UploadEntryBtn.Size = new System.Drawing.Size(176, 53);
+            this.UploadEntryBtn.TabIndex = 11;
+            this.UploadEntryBtn.Text = "Upload Entry";
+            this.UploadEntryBtn.UseVisualStyleBackColor = false;
             // 
             // AddEntry
             // 
@@ -47,6 +62,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(249)))), ((int)(((byte)(243)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UploadEntryBtn);
             this.Controls.Add(this.comboBox1);
             this.Name = "AddEntry";
             this.Text = "AddEntry";
@@ -58,5 +74,6 @@
         #endregion
 
         private ComboBox comboBox1;
+        private Button UploadEntryBtn;
     }
 }
--- SQL Coursework/Menu.cs
+++ SQL Coursework/Menu.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace SQL_Coursework
 {
@@ -19,13 +21,62 @@ namespace SQL_Coursework
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Reports next = new Reports();
-            next.Show();
-            this.Hide();
+            string username = GetStoredUsername();
+            string password = GetStoredPassword(username);
+            if (IsAdmin(username, password))
+            {
+                MessageBox.Show("Access granted. Welcome Admin!");
+                Reports next = new Reports();
+                next.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Access denied. You do not have admin privileges.", "Access Denied", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private string GetStoredUsername()
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.UserAuthConnectionString))
+            {
+                string query = "SELECT TOP 1 Username FROM Users ORDER BY Username"; // Modify query as needed
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                object result = cmd.ExecuteScalar();
+                return result != null ? result.ToString() : string.Empty;
+            }
+        }
+
+        private string GetStoredPassword(string username)
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.UserAuthConnectionString))
+            {
+                string query = "SELECT Password FROM Users WHERE Username=@username";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                conn.Open();
+                object result = cmd.ExecuteScalar();
+                return result != null ? result.ToString() : string.Empty;
+            }
+        }
+        private bool IsAdmin(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.UserAuthConnectionString))
+            {
+                string query = "SELECT AdminID FROM Users WHERE Username=@username AND Password=@password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                conn.Open();
+                object result = cmd.ExecuteScalar();
+                return result != null && result.ToString() == "admin123";
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+
             AddEntry next = new AddEntry();
             next.Show();
             this.Hide();
