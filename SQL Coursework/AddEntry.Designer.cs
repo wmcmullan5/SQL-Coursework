98e9d046a14d62163f5c3005990a65e26e6fde9b
wmcmullan5 <williammcmullan374@gmail.com>
2025-03-18 12:32:56 +0000

    poop

--- SQL Coursework/AddEntry.Designer.cs
+++ SQL Coursework/AddEntry.Designer.cs
@@ -29,10 +29,14 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.UploadEntryBtn = new System.Windows.Forms.Button();
+            this.jobEntryBtn = new System.Windows.Forms.Button();
             this.JobDateBox = new System.Windows.Forms.MonthCalendar();
             this.JobIdtxtbox = new System.Windows.Forms.TextBox();
             this.JobDescriptiontxtbox = new System.Windows.Forms.TextBox();
+            this.VehiclesBtn = new System.Windows.Forms.Button();
+            this.Maint_logBtn = new System.Windows.Forms.Button();
+            this.StaffBtn = new System.Windows.Forms.Button();
+            this.StockBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -45,19 +49,20 @@
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // UploadEntryBtn
+            // jobEntryBtn
             // 
-            this.UploadEntryBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
-            this.UploadEntryBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.UploadEntryBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.UploadEntryBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.UploadEntryBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.UploadEntryBtn.Location = new System.Drawing.Point(312, 348);
-            this.UploadEntryBtn.Name = "UploadEntryBtn";
-            this.UploadEntryBtn.Size = new System.Drawing.Size(176, 53);
-            this.UploadEntryBtn.TabIndex = 11;
-            this.UploadEntryBtn.Text = "Upload Entry";
-            this.UploadEntryBtn.UseVisualStyleBackColor = false;
+            this.jobEntryBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
+            this.jobEntryBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.jobEntryBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.jobEntryBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.jobEntryBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.jobEntryBtn.Location = new System.Drawing.Point(312, 348);
+            this.jobEntryBtn.Name = "jobEntryBtn";
+            this.jobEntryBtn.Size = new System.Drawing.Size(176, 53);
+            this.jobEntryBtn.TabIndex = 11;
+            this.jobEntryBtn.Text = "Upload Entry";
+            this.jobEntryBtn.UseVisualStyleBackColor = false;
+            this.jobEntryBtn.Click += new System.EventHandler(this.UploadEntryBtn_Click);
             // 
             // JobDateBox
             // 
@@ -80,15 +85,79 @@
             this.JobDescriptiontxtbox.Size = new System.Drawing.Size(252, 162);
             this.JobDescriptiontxtbox.TabIndex = 20;
             // 
+            // VehiclesBtn
+            // 
+            this.VehiclesBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
+            this.VehiclesBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.VehiclesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VehiclesBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VehiclesBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.VehiclesBtn.Location = new System.Drawing.Point(312, 348);
+            this.VehiclesBtn.Name = "VehiclesBtn";
+            this.VehiclesBtn.Size = new System.Drawing.Size(176, 53);
+            this.VehiclesBtn.TabIndex = 21;
+            this.VehiclesBtn.Text = "Upload Entry";
+            this.VehiclesBtn.UseVisualStyleBackColor = false;
+            this.VehiclesBtn.Click += new System.EventHandler(this.VehiclesBtn_Click);
+            // 
+            // Maint_logBtn
+            // 
+            this.Maint_logBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
+            this.Maint_logBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Maint_logBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Maint_logBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Maint_logBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Maint_logBtn.Location = new System.Drawing.Point(312, 348);
+            this.Maint_logBtn.Name = "Maint_logBtn";
+            this.Maint_logBtn.Size = new System.Drawing.Size(176, 53);
+            this.Maint_logBtn.TabIndex = 22;
+            this.Maint_logBtn.Text = "Upload Entry";
+            this.Maint_logBtn.UseVisualStyleBackColor = false;
+            this.Maint_logBtn.Click += new System.EventHandler(this.Maint_logBtn_Click);
+            // 
+            // StaffBtn
+            // 
+            this.StaffBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
+            this.StaffBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.StaffBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StaffBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StaffBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StaffBtn.Location = new System.Drawing.Point(312, 348);
+            this.StaffBtn.Name = "StaffBtn";
+            this.StaffBtn.Size = new System.Drawing.Size(176, 53);
+            this.StaffBtn.TabIndex = 23;
+            this.StaffBtn.Text = "Upload Entry";
+            this.StaffBtn.UseVisualStyleBackColor = false;
+            this.StaffBtn.Click += new System.EventHandler(this.StaffBtn_Click);
+            // 
+            // StockBtn
+            // 
+            this.StockBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(191)))), ((int)(((byte)(133)))));
+            this.StockBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.StockBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StockBtn.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.StockBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.StockBtn.Location = new System.Drawing.Point(312, 348);
+            this.StockBtn.Name = "StockBtn";
+            this.StockBtn.Size = new System.Drawing.Size(176, 53);
+            this.StockBtn.TabIndex = 24;
+            this.StockBtn.Text = "Upload Entry";
+            this.StockBtn.UseVisualStyleBackColor = false;
+            this.StockBtn.Click += new System.EventHandler(this.StockBtn_Click);
+            // 
             // AddEntry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(249)))), ((int)(((byte)(243)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StockBtn);
+            this.Controls.Add(this.StaffBtn);
+            this.Controls.Add(this.Maint_logBtn);
+            this.Controls.Add(this.VehiclesBtn);
             this.Controls.Add(this.JobDescriptiontxtbox);
             this.Controls.Add(this.JobIdtxtbox);
-            this.Controls.Add(this.UploadEntryBtn);
+            this.Controls.Add(this.jobEntryBtn);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.JobDateBox);
             this.Name = "AddEntry";
@@ -102,9 +171,13 @@
         #endregion
 
         private ComboBox comboBox1;
-        private Button UploadEntryBtn;
+        private Button jobEntryBtn;
         private MonthCalendar JobDateBox;
         private TextBox JobIdtxtbox;
         private TextBox JobDescriptiontxtbox;
+        private Button VehiclesBtn;
+        private Button Maint_logBtn;
+        private Button StaffBtn;
+        private Button StockBtn;
     }
 }
--- SQL Coursework/AddEntry.cs
+++ SQL Coursework/AddEntry.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SQL_Coursework
 {
@@ -61,28 +62,111 @@ namespace SQL_Coursework
         }
         private void Vehicles()
         {
-
+            Maint_logBtn.Hide();
+            StaffBtn.Hide();
+            jobEntryBtn.Hide();
+            StockBtn.Hide();
+            JobDateBox.Hide();
+            JobDescriptiontxtbox.Hide();
+            JobIdtxtbox.Hide();
         }
         private void MaintenanceLog()
         {
-
+            VehiclesBtn.Hide();
+            StaffBtn.Hide();
+            jobEntryBtn.Hide();
+            StockBtn.Hide();
+            JobDateBox.Hide();
+            JobDescriptiontxtbox.Hide();
+            JobIdtxtbox.Hide();
         }
         private void Staff()
         {
-
+            Maint_logBtn.Hide();
+            VehiclesBtn.Hide();
+            jobEntryBtn.Hide();
+            StockBtn.Hide();
+            JobDateBox.Hide();
+            JobDescriptiontxtbox.Hide();
+            JobIdtxtbox.Hide();
         }
         private void Job()
         {
             JobIdtxtbox.Show();
             JobDescriptiontxtbox.Show();
             JobDateBox.Show();
+            Maint_logBtn.Hide();
+            StaffBtn.Hide();
+            VehiclesBtn.Hide();
+            StockBtn.Hide();
         }
         private void Stock()
+        {
+            Maint_logBtn.Hide();
+            StaffBtn.Hide();
+            jobEntryBtn.Hide();
+            VehiclesBtn.Hide();
+            JobDateBox.Hide();
+            JobDescriptiontxtbox.Hide();
+            JobIdtxtbox.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void UploadEntryBtn_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Job (jobID, jobDate, jobDescription) VALUES (@jobID, @jobDate, @jobDescription)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        // Adding parameters
+                        cmd.Parameters.AddWithValue("@jobID", int.Parse(JobIdtxtbox.Text));
+                        cmd.Parameters.AddWithValue("@jobDate", JobDateBox.SelectionStart);
+                        cmd.Parameters.AddWithValue("@jobDescription", JobDescriptiontxtbox.Text);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data inserted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Insertion failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}", "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void StockBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VehiclesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StaffBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Maint_logBtn_Click(object sender, EventArgs e)
         {
 
         }
